ca45bcadbcaeb721fd0a1c7db7b45b240b8a175b
Ankit Gupta <ag3831124@gmail.com>
2020-07-08 23:59:07 +0530

    some changes

--- pages/index.js
+++ pages/index.js
@@ -67,12 +67,12 @@ export default function Home() {
   
       <div id="mc_embed_signup">
          <form noValidate>
-               <label for="mce-EMAIL">
-            <input type="email" value={state.email} name="email" className="email" onChange={(e)=> { setstate({email:e.target.value})}} id="mce-EMAIL" placeholder="email address" required />
-            </label>
-            <div style={{position: "absolute", left: "-5000px"}}><input type="text" name="b_cdb7b577e41181934ed6a6a44_e65110b38d"  /></div>
+       
+            <input type="email" aria-label="email" value={state.email} name="email" className="email" onChange={(e)=> { setstate({email:e.target.value})}} id="mce-EMAIL" placeholder="email address" required />
+         
+            <div style={{position: "absolute", left: "-5000px"}}><input type="text" name="b_cdb7b577e41181934ed6a6a44_e65110b38d" aria-label="nothing"  /></div>
             
-         <input type="submit" onClick={(e) =>{e.preventDefault(); onEmail(state.email)}} value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/>
+         <input type="submit" aria-label="button" onClick={(e) =>{e.preventDefault(); onEmail(state.email)}} value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/>
             
          </form>
       </div>
