641c7c31f3ca64df902cb5520a753ec85174c9e9
Ankit Gupta <ag3831124@gmail.com>
2020-07-10 02:18:06 +0530

    page changed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import {useState} from 'react'
+import fetch from 'node-fetch'
 export default function Home() {
 
  const [state, setstate] = useState({email:''})
@@ -33,65 +34,123 @@ export default function Home() {
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5"/>
 
 
-<link rel="stylesheet" href="css/default.css"/>
-<link rel="stylesheet" href="css/layout.css"/>
-<link rel="stylesheet" href="css/media-queries.css"/>    
+<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css"/>
 
-<script src="js/modernizr.js"></script>
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css"/>
 
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css"/>
+
+	<link rel="stylesheet" type="text/css" href="css/util.css"/>
+	<link rel="stylesheet" type="text/css" href="css/main.css"></link>
 <link rel="shortcut icon" href="favicon.png" />
+<script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
    </Head>
-      <section id="intro">
+   <body>
+   <div className="simpleslide100">
+	
+		<div className="simpleslide100-item bg-img1" style={{backgroundImage: "url('image1.png')"}}></div>
+	</div>
 
-   	
-<div  id="main" className="row">
-
-
-  <div className="twelve columns">
+	<div className="size1 overlay1">
+		{/* <!--  --> */}
+    <nav className="mb-1 navbar navbar-expand-lg navbar-dark secondary-color lighten-1">
+      <a className="navbar-brand" href="blog.99smartphones.in"><img style={{width:"80px"}} src="android-chrome-192x192.png" alt="LOGO"/></a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-5" aria-controls="navbarSupportedContent-5" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent-5">
+        <ul className="navbar-nav navbar-right mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link waves-effect waves-light" href="#">Home
+              <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link waves-effect waves-light" href="https://blog.99smartphones.in">Blogs</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link waves-effect waves-light" href="https://blog.99smartphones.in">News</a>
+          </li>
       
-      <img src="android-chrome-192x192.png" alt='Image' />
-    <h1>We are currently working on something awesome. Stay tuned!</h1>
-
-    {/* <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-
-    <h5>Time Left Until Launching</h5>
-
-    <div id="counter" class="cf">
-      <span>134 <em>days</em></span> 
-      <span>12 <em>hours</em></span>
-     <span>50 <em>minutes</em></span>
-      <span>33 <em>seconds</em></span> 
-    </div>					 */}
-
-  
-      <div id="mc_embed_signup">
-         <form noValidate>
-       
-            <input type="email" aria-label="email" value={state.email} name="email" className="email" onChange={(e)=> { setstate({email:e.target.value})}} id="mce-EMAIL" placeholder="Email Address" required />
-         
-            <div style={{position: "absolute", left: "-5000px"}}><input type="text" name="b_cdb7b577e41181934ed6a6a44_e65110b38d" aria-label="nothing"  /></div>
-            
-         <input type="submit" aria-label="button" onClick={(e) =>{e.preventDefault(); onEmail(state.email)}} value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/>
-            
-         </form>
+        </ul>
+     
       </div>
+    </nav>
+		{/* <!--  --> */}
+    <div style={{height:"110px"}}></div>
+		<div className="flex-sb flex-row-rev where3-parent p-l-58 p-r-46 respon2">
+			{/* <!--  --> */}
+			<div className="where3 wsize2 respon1">
+				<h3 className="l1-txt2 p-b-30 respon6 respon7">
+					Coming Soon
+				</h3>
 
-      <ul className="social">
-         <li><a href="https://www.facebook.com/99smartphones.in" aria-label="facebook_link"><i className="fa fa-facebook"></i></a></li>
-         {/* <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-         <li><a href="#"><i class="fa fa-google-plus"></i></a></li> */}
-         {/* <li><a href="#"><i class="fa fa-linkedin"></i></a></li> */}
-         <li><a href="https://www.instagram.com/99s_martphones/?hl=en" aria-label="insta_link"><i className="fa fa-instagram"></i></a></li>
-         {/* <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-         <li><a href="#"><i class="fa fa-skype"></i></a></li> */}
-       </ul>
+				<p className="m2-txt1 respon6">
+					Follow us for update now!
+				</p>
 
-    </div> 
+				<form className="contact100-form validate-form p-t-55 w-full">
+					<div className="wrap-input100 validate-input m-lr-auto-lg" data-validate = "Email is required: ex@abc.xyz">
+						<input className="s2-txt1 placeholder0 input100 trans-04" type="text" onChange={(e) => {setstate({email: e.target.value})}} name="email" placeholder="Email Address"/>
 
- </div> 
+						<button ria-label="Email Button" type="button" onClick={onEmail(state.email)} className="flex-c-m ab-t-r size4 s1-txt1 hov1 trans-04">
+							<i className="fa fa-paper-plane fs-15 cl0"></i>
+						</button>
+					</div>		
+				</form>		
+			</div>
+  
+			{/* <!--  --> */}
+			{/* <div className="flex-w flex-col cd100 p-t-34 respon3">
+				<div className="flex-col wsize1 m-b-40 respon4">
+					<span className="l1-txt1 p-b-30 days">35</span>
+					<span className="s1-txt1">Days</span>
+				</div>
 
-</section> 
+				<div className="flex-col wsize1 m-b-40 respon4">
+					<span className="l1-txt1 p-b-30 hours">17</span>
+					<span className="s1-txt1">Hours</span>
+				</div>
+
+				<div className="flex-col wsize1 m-b-40 respon4">
+					<span className="l1-txt1 p-b-30 minutes">50</span>
+					<span className="s1-txt1">Minutes</span>
+				</div>
+
+				<div className="flex-col wsize1 m-b-40 respon4">
+					<span className="l1-txt1 p-b-30 seconds">39</span>
+					<span className="s1-txt1">Seconds</span>
+				</div>
+			</div> */}
+{/* 
+			<!--  --> */}
+  
+			<div className="flex-w flex-col cd100 p-t-34 respon3">
+				<a href="https://www.facebook.com/99smartphones.in" aria-label="facebook_link" className="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+					<i className="fa fa-facebook-official"></i>
+				</a>
+
+				<a  href="https://www.instagram.com/99s_martphones/?hl=en" aria-label="insta_link" className="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+					<i className="fa fa-instagram"></i>
+				</a>
+
+				<a href="#" aria-label="youtube_link"  className="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+					<i className="fa fa-youtube"></i>
+				</a>
+			</div>
+
+		</div>
+    </div>
+
+    </body>
     </html>
   )
 }
